1c88f06cf4dcc1761676a8bcc13bb8c09555d085
AoElite <20812471+AoElite@users.noreply.github.com>
2024-12-29 22:27:07 -0500

    more changes

--- src/main/java/ac/grim/grimac/api/AbstractProcessor.java
+++ src/main/java/ac/grim/grimac/api/AbstractProcessor.java
@@ -2,12 +2,19 @@ package ac.grim.grimac.api;
 
 import ac.grim.grimac.api.checks.ListenerType;
 import ac.grim.grimac.api.common.BasicReloadable;
+import ac.grim.grimac.api.config.ConfigReloadable;
 
-public interface AbstractProcessor extends BasicReloadable {
-
+public interface AbstractProcessor extends BasicReloadable, ConfigReloadable {
+    // name of the check
     String getConfigName();
-
+    // listener type of the check
     ListenerType getListenerType();
+    // if the player supports the check
+    boolean isSupported();
+    // how many listeners
+    int getListeners();
+
+    void setListeners(int listeners);
 
 }
 
--- src/main/java/ac/grim/grimac/api/checks/AbstractCheckManager.java
+++ src/main/java/ac/grim/grimac/api/checks/AbstractCheckManager.java
@@ -9,14 +9,31 @@ import java.util.Collection;
 
 public interface AbstractCheckManager {
 
-    <T extends AbstractProcessor> void registerProcessor(@NotNull Class<T> clazz, @NotNull T processor, @NotNull ListenerType listenerType);
+    /**
+     * Register a processor
+     * @param clazz The class of the processor
+     * @param processor The processor
+     * @return The number of listeners registered
+     */
+    <T extends AbstractProcessor> int registerProcessor(@NotNull Class<T> clazz, @NotNull T processor);
+
+    /**
+     * Unregister a processor
+     * @param processor The processor
+     */
 
     void unregisterProcessor(AbstractProcessor processor);
 
+    /**
+     * Get all processors
+     * @return All processors
+     */
     Collection<? extends AbstractProcessor> getAllProcessors();
 
+    /**
+     * Get all checks
+     * @return All checks
+     */
     Collection<? extends AbstractCheck> getChecks();
-
-    <T extends AbstractProcessor> T getProcessor(@NotNull Class<T> check);
 
 }
