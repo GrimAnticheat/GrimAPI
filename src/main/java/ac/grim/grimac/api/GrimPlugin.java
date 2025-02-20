f1eff912b617fcd30745520df479e53796400f73
Axionize <154778082+Axionize@users.noreply.github.com>
2025-02-20 03:20:13 -0500

    Add methods to GrimPlugin

--- src/main/java/ac/grim/grimac/api/GrimPlugin.java
+++ src/main/java/ac/grim/grimac/api/GrimPlugin.java
@@ -1,4 +1,13 @@
 package ac.grim.grimac.api;
 
+import java.io.File;
+import java.util.logging.Logger;
+
 public interface GrimPlugin {
+
+    GrimPluginDescription getDescription();
+
+    Logger getLogger();
+
+    File getDataFolder();
 }
