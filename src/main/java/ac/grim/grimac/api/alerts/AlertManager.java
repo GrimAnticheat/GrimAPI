bd5b9deb1743596bb784219b894e929cab139fb0
Axionize <154778082+Axionize@users.noreply.github.com>
2025-02-25 04:42:34 -0500

    Update Alertmanager with new methods and documentation

--- src/main/java/ac/grim/grimac/api/alerts/AlertManager.java
+++ src/main/java/ac/grim/grimac/api/alerts/AlertManager.java
@@ -1,33 +1,68 @@
 package ac.grim.grimac.api.alerts;
 
 import ac.grim.grimac.api.GrimUser;
+import lombok.NonNull;
 
 public interface AlertManager {
 
     /**
      * Checks if the player has alerts enabled.
-     * @param player
-     * @return boolean
+     *
+     * @param player The GrimUser to check
+     * @return true if the player has alerts enabled, false otherwise
+     * @throws NullPointerException if player is null
      */
-    boolean hasAlertsEnabled(GrimUser player);
+    boolean hasAlertsEnabled(@NonNull GrimUser player);
 
     /**
      * Toggles alerts for the player.
-     * @param player
+     * If alerts are currently disabled, they will be enabled.
+     * If alerts are currently enabled, they will be disabled.
+     *
+     * @param player The GrimUser to toggle alerts for
+     * @return true if alerts are now enabled, false if alerts are now disabled
+     * @throws NullPointerException if player is null
      */
-    void toggleAlerts(GrimUser player);
+    boolean toggleAlerts(@NonNull GrimUser player);
 
     /**
      * Checks if the player has verbose enabled.
-     * @param player
-     * @return boolean
+     *
+     * @param player The GrimUser to check
+     * @return true if the player has verbose enabled, false otherwise
+     * @throws NullPointerException if player is null
      */
-    boolean hasVerboseEnabled(GrimUser player);
+    boolean hasVerboseEnabled(@NonNull GrimUser player);
 
     /**
      * Toggles verbose for the player.
-     * @param player
+     * If verbose is currently disabled, it will be enabled.
+     * If verbose is currently enabled, it will be disabled.
+     *
+     * @param player The GrimUser to toggle verbose for
+     * @return true if verbose is now enabled, false if verbose is now disabled
+     * @throws NullPointerException if player is null
      */
-    void toggleVerbose(GrimUser player);
+    boolean toggleVerbose(@NonNull GrimUser player);
 
+    /**
+     * Checks if the player has brand notifications enabled.
+     *
+     * @param player The GrimUser to check
+     * @return true if the player has brand notifications enabled and has the "grim.brand" permission,
+     *         false otherwise
+     * @throws NullPointerException if player is null
+     */
+    boolean hasBrandsEnabled(@NonNull GrimUser player);
+
+    /**
+     * Toggles brand notifications for the player.
+     * If brand notifications are currently disabled, they will be enabled.
+     * If brand notifications are currently enabled, they will be disabled.
+     *
+     * @param player The GrimUser to toggle brand notifications for
+     * @return true if brand notifications are now enabled, false if brand notifications are now disabled
+     * @throws NullPointerException if player is null
+     */
+    boolean toggleBrands(@NonNull GrimUser player);
 }
