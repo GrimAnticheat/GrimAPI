7eeba92b9edcf8fa6f9e2e11769cc77acdb00742
Axionize <154778082+Axionize@users.noreply.github.com>
2026-01-07 21:15:59 -0500

    Bump version

--- src/main/java/ac/grim/grimac/api/event/events/GrimUserEvent.java
+++ src/main/java/ac/grim/grimac/api/event/events/GrimUserEvent.java
@@ -1,9 +1,6 @@
 package ac.grim.grimac.api.event.events;
 
-import ac.grim.grimac.api.AbstractCheck;
 import ac.grim.grimac.api.GrimUser;
-import ac.grim.grimac.api.event.GrimEvent;
-import ac.grim.grimac.api.event.Cancellable;
 
 public interface GrimUserEvent {
     GrimUser getUser();
--- src/main/java/ac/grim/grimac/api/event/events/GrimVerboseCheckEvent.java
+++ src/main/java/ac/grim/grimac/api/event/events/GrimVerboseCheckEvent.java
@@ -2,9 +2,6 @@ package ac.grim.grimac.api.event.events;
 
 import ac.grim.grimac.api.AbstractCheck;
 import ac.grim.grimac.api.GrimUser;
-import ac.grim.grimac.api.event.Cancellable;
-import ac.grim.grimac.api.event.GrimEvent;
-import lombok.Getter;
 
 public abstract class GrimVerboseCheckEvent extends GrimCheckEvent {
     private final String verbose;
